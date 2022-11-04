bea5b3d8f097217e3c7dd5b9102106e01c54fae9
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2022-11-04 18:11:25 +0300

    Added get request for gym

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import auth, profiles, role, gender, example, workout_type, workout
+from app.routers import auth, profiles, role, gender, example, workout_type, workout, gym
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import os
@@ -27,6 +27,7 @@ app.include_router(role.router)
 app.include_router(gender.router)
 app.include_router(workout_type.router)
 
+app.include_router(gym.router)
 app.include_router(workout.router)
 
 app.include_router(example.router)
