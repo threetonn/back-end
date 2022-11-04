29090992a597d61c959de0289f7855efdef3eb37
skachkovkirill <skachkovkiril@yandex.ru>
2022-11-04 17:42:04 +0300

    Added profile

--- app/routers/profiles.py
+++ app/routers/profiles.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import Session
 from app.permissions import get_current_user
 from app.models import User
 from app.upload_image import handle_file_upload
-from app.schemas.profiles import ClientBase, TrainerBase
-from app.utils.profiles import get_trainer_profile
+from app.schemas.profiles import ClientBase, TrainerBase, EditUser, EditTrainer
+from app.utils.profiles import get_trainer_profile, get_user_profile, edit_trainer_profile, edit_user_profile
+
 
 router = APIRouter(
     tags=["Profile"],
@@ -25,5 +26,16 @@ async def add_image(db: Session = Depends(get_db), image: UploadFile = File(...)
 @router.get('/me', response_model=TrainerBase | ClientBase)
 def profile(user: User = Security(get_current_user)):
     if user.Role.name == "trainer":
-        return get_trainer_profile(user=user)
-    return user
+        return get_trainer_profile(user)
+    return get_user_profile(user)
+
+
+@router.put('/edit', response_model=TrainerBase | ClientBase)
+def edit_profile(new_data: EditTrainer | EditUser, db: Session = Depends(get_db), user: User = Security(get_current_user)):
+    if user.Role.name == "trainer":
+        user = edit_trainer_profile(db, user, new_data)
+        return get_trainer_profile(user)
+    else: 
+        user = edit_user_profile(db, user, new_data)
+        return get_user_profile(user)
+    
