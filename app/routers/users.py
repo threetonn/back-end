d07f5212aa18e4735818bea031dbb5b6b06c61b0
skachkovkirill <skachkovkiril@yandex.ru>
2022-11-16 16:31:18 +0300

    Added user (get clients, trainers, managers)

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from app.database import get_db
 from sqlalchemy.orm import Session
-from app.utils.users import get_trainers_db
-from app.schemas.users import TrainerBase
+from app.utils.users import get_trainers_db, get_clients_db, get_managers_db
+from app.schemas.users import TrainerBase, ClientBase, ManagerBase
 
 
 router = APIRouter(
@@ -12,5 +12,15 @@ router = APIRouter(
 
 
 @router.get('/trainers', response_model=list[TrainerBase])
-def get_trainers(db: Session = Depends(get_db)):
-    return get_trainers_db(db=db)
+def get_trainers(request: Request, page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
+    return get_trainers_db(db=db, request=request, limit=limit, page=page)
+
+
+@router.get('/managers', response_model=list[ManagerBase])
+def get_managers(request: Request, page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
+    return get_managers_db(db=db, request=request, limit=limit, page=page)
+
+
+@router.get('/clients', response_model=list[ClientBase])
+def get_clients(request: Request, page: int = 1, limit: int = 10, search: str = None, db: Session = Depends(get_db)):
+    return get_clients_db(db=db, request=request, limit=limit, page=page, search=search)
--- app/utils/users.py
+++ app/utils/users.py
@@ -1,10 +1,37 @@
 from app.models import User, Role
 from sqlalchemy.orm import Session
+from sqlalchemy import func, case
+from fastapi import Request
 
 
-def get_trainers_db(db: Session):
+def get_trainers_db(db: Session, request: Request, page: int, limit: int):
     """ Возвращает список всех тренеров """
     trainers = db.query(User).join(Role).filter(User.Role_id == Role.id).filter(Role.name == "trainer").all()
     for i in trainers:
         i.workout_type = [i.description for i in i.WorkoutTypes]
-    return trainers
+        i.image = f"http://{ request.url.hostname }:{ request.url.port }{ i.image }" if i.image else None
+    return trainers[(page - 1) * limit : page * limit]
+
+
+def get_managers_db(db: Session, request: Request, page: int, limit: int):
+    """ Возвращает список всех менеджеров """
+    trainers = db.query(User).join(Role).filter(User.Role_id == Role.id).filter(Role.name == "manager").all()
+    for i in trainers:
+        i.position = i.bio
+        i.image = f"http://{ request.url.hostname }:{ request.url.port }{ i.image }" if i.image else None
+    return trainers[(page - 1) * limit : page * limit]
+
+
+def get_clients_db(db: Session, request: Request, page: int, limit: int, search: str):
+    """ Возвращает список всех клиентов """
+    clients = db.query(User).join(Role).filter(User.Role_id == Role.id, Role.name == "client")
+    if search:
+        clients = clients.filter(func.concat(User.surname, " ", User.name, " ", User.patronymic).label("full_name").contains(search))
+    clients = clients.all()
+    for i in clients:
+        i.image = f"http://{ request.url.hostname }:{ request.url.port }{ i.image }" if i.image else None
+        i.gender = {
+            "ru": "Мужчина" if i.Gender.name == "male" else "Женщина",
+            "en": i.Gender.name
+        }
+    return clients[(page - 1) * limit : page * limit]
