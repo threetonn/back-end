8aafb846e110f85c06b71088475387d40406cb4c
skachkovkirill <skachkovkiril@yandex.ru>
2022-11-16 16:35:35 +0300

    Added func for subscriptions

--- app/utils/subscription.py
+++ app/utils/subscription.py
@@ -1,24 +1,35 @@
-from app.models import Subscription
-from sqlalchemy.orm import Session
+from app.models import Subscription, Feature, Workouttype
+from sqlalchemy.orm import Session, aliased
+from fastapi import HTTPException
 
 
-def get_subscriptions_db(db: Session):
-    """ Возвращает список подписок """
-    subscriptions = db.query(Subscription).filter(Subscription.is_active == True).all()
-    for i in subscriptions:
-        i.features = [j.name for j in i.Features] + [j.description for j in i.WorkoutTypes]
-        i.price = i.SubscriptionDuration.price
-        i.discount = i.SubscriptionDuration.discount
-        i.type = i.SubscriptionType.type
-        i.title = i.SubscriptionType.name
-    return subscriptions
-
-def get_subscription_db(db: Session, id: int):
-    """ Возвращает текущую подписку польователя """
-    subscription = db.query(Subscription).filter(Subscription.is_active == True).filter(Subscription.id == id).first()
+def add_a_field(subscription: Subscription):
     subscription.features = [j.name for j in subscription.Features] + [j.description for j in subscription.WorkoutTypes]
     subscription.price = subscription.SubscriptionDuration.price
     subscription.discount = subscription.SubscriptionDuration.discount
     subscription.type = subscription.SubscriptionType.type
     subscription.title = subscription.SubscriptionType.name
     return subscription
+
+
+def get_subscriptions_db(db: Session):
+    """ Возвращает список подписок """
+    subscriptions = db.query(Subscription).filter(Subscription.is_active).all()
+    for i in subscriptions:
+        i = add_a_field(subscription=i)
+    return subscriptions
+
+def get_subscription_db(db: Session, id: int):
+    """ Возвращает текущую подписку польователя """
+    subscription = db.query(Subscription).filter(Subscription.is_active).filter(Subscription.id == id).first()
+    if subscription:
+        return add_a_field(subscription=subscription)
+    raise HTTPException(status_code=404, detail="Subscription is not found")
+
+
+def get_features_db(db: Session):
+    features_db = db.query(Feature.id, Feature.name).all()
+    features = [ {"id": i[0], "name": i[1], "type": "features"} for i in features_db ]
+    workouttypes_db = db.query(Workouttype.id, Workouttype.description).all()
+    workouttypes = [ {"id": i[0], "name": i[1], "type": "workouttypes"} for i in workouttypes_db ]
+    return features + workouttypes
