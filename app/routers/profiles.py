c5ef79211afdae0a4947b14afc3573f4cb51ab14
skachkovkirill <skachkovkiril@yandex.ru>
2022-11-16 16:36:50 +0300

    Edited response func

--- app/routers/profiles.py
+++ app/routers/profiles.py
@@ -16,16 +16,18 @@ router = APIRouter(
 
 @router.post('/add_image', response_model=TrainerBase | ClientBase)
 async def add_image(
-    request: Request,
-    db: Session = Depends(get_db), 
-    image: UploadFile = File(...), 
-    user: User = Security(get_current_user)
+        request: Request,
+        db: Session = Depends(get_db), 
+        image: UploadFile = File(...), 
+        user: User = Security(get_current_user)
     ):
     user.image = await handle_file_upload(id=user.id, file=image)
     db.add(user)
     db.commit()
     db.refresh(user)
-    return get_trainer_profile(request, user) if user.Role.name == "trainer" else get_user_profile(request, user=user, db=db)
+    if user.Role.name == "trainer":
+        return get_trainer_profile(request, user)  
+    return get_user_profile(request, user=user, db=db)
 
 
 @router.get('/me', response_model=TrainerBase | ClientBase)
@@ -36,7 +38,12 @@ def profile(request: Request, user: User = Security(get_current_user), db: Sessi
 
 
 @router.put('/edit', response_model=TrainerBase | ClientBase)
-def edit_profile(request: Request,new_data: EditTrainer | EditUser, db: Session = Depends(get_db), user: User = Security(get_current_user)):
+def edit_profile(
+        request: Request,
+        new_data: EditTrainer | EditUser, 
+        db: Session = Depends(get_db),
+        user: User = Security(get_current_user)
+    ):
     if user.Role.name == "trainer":
         user = edit_trainer_profile(db, user, new_data)
         return get_trainer_profile(request, user)
